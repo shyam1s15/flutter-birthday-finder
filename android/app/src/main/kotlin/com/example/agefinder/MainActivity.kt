6493defa2957b81cdb1f37d07392a44974d9246d
shyam kachhadiya <shyam1ss15@gmail.com>
2022-10-10 01:51:12 +0530

    flutter site to find birthday

--- android/app/src/main/kotlin/com/example/agefinder/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.agefinder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:agefinder/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AgeFinder());
+    // await tester.pumpWidget(const AgeFinder());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
